6a1d6ccbaa2502d73b4c05f40f55cd4c81bff770
ProgressAce <daiyaansallie7@gmail.com>
2023-11-05 02:55:56 -0800

    style: correct method name

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -17,7 +17,7 @@ class Square():
         """Area of the square."""
         return self.width * self.height
 
-    def perimeter_of_my_square(self):
+    def permiter_of_my_square(self):
         """Returns the perimeter of the square."""
         return (self.width * 2) + (self.height * 2)
 
